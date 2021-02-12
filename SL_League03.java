c8180692ce9632ebb3063ef4ad9b13e39da8d151
Balakrishnan Sivasowmian <65554181+Sivasowmian@users.noreply.github.com>
2021-02-12 17:48:23 +0530

    ToIntFunction mapper
    Returns an IntStream consisting of the results of applying the given function to the elements of this stream.

---
+++ SL_League03.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.OptionalInt;
+
+
+public class SL_League03 {
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		ArrayList<SL_Club> table = new ArrayList();
+		
+		String line = "";  
+		String splitBy = ",";  
+		try   
+		{  
+		//parsing a CSV file into BufferedReader class constructor  
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\User\\OneDrive\\Desktop\\SL_Rugby.csv"));  
+		
+		while ((line = br.readLine()) != null)   //returns a Boolean value  
+		{  
+		String[] club = line.split(splitBy); 
+		SL_Club clubNew=new SL_Club(Integer.parseInt(club[0].replaceAll("\\s+","")),club[1].replaceAll("\\s+",""),Integer.parseInt(club[2].replaceAll("\\s+","")),Integer.parseInt(club[3].replaceAll("\\s+","")),Integer.parseInt(club[4].replaceAll("\\s+","")),Integer.parseInt(club[5].replaceAll("\\s+","")),Integer.parseInt(club[6].replaceAll("\\s+","")),Integer.parseInt(club[7].replaceAll("\\s+","")),Integer.parseInt(club[8].replaceAll("\\s+","")),Integer.parseInt(club[9].replaceAll("\\s+","")),Integer.parseInt(club[10].replaceAll("\\s+","")),Integer.parseInt(club[11].replaceAll("\\s+","")),Integer.parseInt(club[12].replaceAll("\\s+","")),Integer.parseInt(club[13].replaceAll("\\s+","")));
+		
+		table.add(clubNew);
+		}  
+		}   
+		catch (IOException e)   
+		{  
+		e.printStackTrace();  
+		} 
+		
+		OptionalInt min = table.stream().mapToInt(SL_Club::getPoints).min();
+	    if (min.isPresent()) {
+	      System.out.printf("Lowest number of points is %d\n", min.getAsInt());
+	    } else {
+	      System.out.println("min failed");
+	    }
+	  }
+	}
+	
+
+
